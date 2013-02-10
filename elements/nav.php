0f2f07a28676f3684bbf87335d0cc2ebca861ca7
scalarmapedia <chris@scalar.mapedia-uk.net>
2013-02-10 13:07:43 +0000

    Updated to use new icons and icon names

--- elements/nav.php
+++ elements/nav.php
@@ -4,14 +4,14 @@
 		<ul>						
 			<li id="nav_about" class="" onMouseOver="scalar.toggleFilter(event,this,'icon_about',1,.5)" onMouseOut="scalar.toggleFilter(event,this,'icon_about',0,.5)">
 				<a href="<?php echo SITE_HOST ?>/">
-					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_about','black',Array(Array('icon_disc',10,10,0,.8),Array('icon_questionmark',15,15,0,.7,false)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
+					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_about','black',Array(Array('icon_disc',10,10,0,.8),Array('icon_question',25,25,0,.5,false)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
 					<h1>About</h1>
 				</a>
 			</li>
 			
 			<li id="nav_features" class="" onMouseOver="scalar.toggleFilter(event,this,'icon_features',1,.5)" onMouseOut="scalar.toggleFilter(event,this,'icon_features',0,.5)">
 				<a href="<?php echo SITE_HOST ?>/features/">
-					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_features','black',Array(Array('icon_disc',10,10,0,.8),Array('icon_star',15,12,0,.7,false)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
+					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_features','black',Array(Array('icon_disc',10,10,0,.8),Array('icon_star',20,20,0,.6,false)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
 					<h1>Features</h1>
 				</a>
 			</li>
@@ -41,41 +41,6 @@
 				<a href="<?php echo SITE_HOST ?>/docs/">
 					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_docs','black',Array(Array('icon_docs',10,10,0,.8)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
 					<h1>Docs</h1>
-				</a>
-			</li>
-		
-			<li id="nav_twitter" class="hide" onMouseOver="scalar.toggleFilter(event,this,'icon_twitter',1,.5)" onMouseOut="scalar.toggleFilter(event,this,'icon_twitter',0,.5)">
-				<a href="<?php echo SITE_HOST ?>/twitter/">
-					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_twitter','black',Array(Array('web_twitterbird',10,10,0,.8)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
-					<h1>Twitter</h1>
-				</a>
-			</li>
-			
-			<li id="nav_library" class="hide" onMouseOver="scalar.toggleFilter(event,this,'icon_library',1,.5)" onMouseOut="scalar.toggleFilter(event,this,'icon_library',0,.5)">
-				<a href="<?php echo SITE_HOST ?>/library/">
-					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_library','black',Array(Array('icon_disc',10,10,0,.8),Array('icon_scroll',20,20,0,.6,false)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
-					<h1>Icon Library</h1>
-				</a>
-			</li>
-	
-			<li id="nav_buzz" class="hide" onMouseOver="scalar.toggleFilter(event,this,'icon_buzz',1,.5)" onMouseOut="scalar.toggleFilter(event,this,'icon_buzz',0,.5)">
-				<a href="<?php echo SITE_HOST ?>/buzz/">
-					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_buzz','black',Array(Array('icon_disc',10,10,0,.8),Array('icon_comment',16,16,0,.7,false)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
-					<h1>Buzz</h1>
-				</a>
-			</li>
-	
-			<li id="nav_donate" class="hide" onMouseOver="scalar.toggleFilter(event,this,'icon_donate',1,.5)" onMouseOut="scalar.toggleFilter(event,this,'icon_donate',0,.5)">
-				<a href="<?php echo SITE_HOST ?>/donate/">
-					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_donate','black',Array(Array('icon_disc',10,10,0,.8),Array('currency_gbp',19,18,0,.6,false)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
-					<h1>Donate</h1>
-				</a>
-			</li>
-			
-			<li id="nav_download" class="hide" onMouseOver="scalar.toggleFilter(event,this,'icon_download',1,.5)" onMouseOut="scalar.toggleFilter(event,this,'icon_download',0,.5)">
-				<a href="<?php echo SITE_HOST ?>/download/">
-					<img src="/scalar/null.png" onClick="scalar.toggleFilter(event,this);" onLoad="scalar.icon(event,'icon_download','black',Array(Array('icon_disc',10,10,0,.8),Array('icon_arrow',22,18,180,.6,false)),['dropShadow',0,0,9,9,0.9,Array(200,50,4)])">
-					<h1>Download</h1>
 				</a>
 			</li>
 		</ul>
