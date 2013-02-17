ca6d5c9fb3201db65ed4afbac637086f48baf7a0
scalarmapedia <chris@scalar.mapedia-uk.net>
2013-02-17 19:56:33 +0000

    Added link to mapedia on footer, index has test of Video video

--- elements/footer.php
+++ elements/footer.php
@@ -1,3 +1,3 @@
 <footer>
-	<b>Scalar</b> was produced by <i>Chris Thomas</i> as a part of the <b>Mapedia</b> project.
+<b>Scalar</b> was produced by <i>Chris Thomas</i> as a part of the <a href="http://www.mapedia-uk.net"><b>Mapedia-UK</b></a> project.
 </footer>
