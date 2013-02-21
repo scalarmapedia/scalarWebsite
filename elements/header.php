a947904c240432b4c5ee5242804aee52248789a3
scalarmapedia <chris@scalar.mapedia-uk.net>
2013-02-21 17:49:56 +0000

    removed background elements

--- elements/header.php
+++ elements/header.php
@@ -1,13 +1,6 @@
-<gfx>
-	<cont>
-		<img src="/scalar/null.png" onLoad="scalar.icon(event,'circuitboard_body',null,Array(Array('icon_circuitboard',0,0,15,1)))">
-	</cont>
-</gfx>
-		
 <blanker>
 	<logoCont id="scalarlogocont">
-			<logo>
-				
+			<logo>			
 				<img src="/scalar/null.png" class="scalarLogo" onLoad="scalar.icon(event,'scalarLogo',null,Array(Array('icon_scalarlogo',5,5,0,.9)),['glow',1,3,1],null,scalarSetEvents)">
 			</logo>
 	</logoCont>
