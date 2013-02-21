b2e8e89aec27f8316b8f624a634d726f7b173d8c
scalarmapedia <chris@scalar.mapedia-uk.net>
2013-02-21 16:17:55 +0000

    Added the circuitboard pattern  to the bg of main page. Also made Main Scalar logo larger

--- elements/header.php
+++ elements/header.php
@@ -1,25 +1,26 @@
-
-<blanker>
-	<header id="pageHeader">
+<gfx>
+	<cont>
+		<img src="/scalar/null.png" onLoad="scalar.icon(event,'circuitboard_body',null,Array(Array('icon_circuitboard',0,0,15,1)))">
+	</cont>
+</gfx>
 		
-		<logoCont id="scalarlogocont">
+<blanker>
+	<logoCont id="scalarlogocont">
 			<logo>
+				
 				<img src="/scalar/null.png" class="scalarLogo" onLoad="scalar.icon(event,'scalarLogo',null,Array(Array('icon_scalarlogo',5,5,0,.9)),['glow',1,3,1],null,scalarSetEvents)">
 			</logo>
-
-		</logoCont>
+	</logoCont>
 		
+	<header id="pageHeader">
+		<gfx>
+			<img src="/scalar/null.png" onLoad="scalar.icon(event,'circuitboard_header',null,Array(Array('icon_circuitboard',0,0,15,1)))">
+		</gfx>
+			
 		<txtCont>
 			<txt>
 				<?php echo PAGE_NAME; ?>
 			</txt>
 		</txtCont>
-		
-		<gfx>
-			<img src="/scalar/null.png" onLoad="scalar.icon(event,'clocky',null,[['icon_circuitboard',0,0,15,1]])">
-		</gfx>
 	</header>
-	
-	<fadeout>
-	</fadeout>
 </blanker>
